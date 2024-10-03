cc8d8103c284e76e4920554fb0e051c244e8875f
Daniel-1125 <javierdaniel1125@gmail.com>
2024-10-03 15:55:10 +0800

    dawn

---
+++ js/slider.js
@@ -0,0 +1,41 @@
+var slider = tns({
+  container: ".banner__slider",
+  items: 1,
+  speed: 200,
+  gutter: 100,
+  slideBy: "page",
+  autoplay: true,
+  navPosition: "bottom",
+  // containerControls: "#controls",
+  // prevButton: ".prev",
+  // nextButton: ".next",
+  responsive: {
+    1250: {
+      items: 1,
+    },
+    200: {
+      items: 1,
+    },
+  },
+});
+
+var slider = tns({
+  container: ".services__slider",
+  items: 3,
+  speed: 200,
+  gutter: 100,
+  slideBy: "page ",
+  autoplay: true,
+  navPosition: "bottom",
+  // containerControls: "#controls",
+  // prevButton: ".prev",
+  // nextButton: ".next",
+  responsive: {
+    1250: {
+      items: 1,
+    },
+    200: {
+      items: 1,
+    },
+  },
+});
