e770aac6dfbfb80fe4f16a57f874ec9a30c7cdf1
tomatofrog1 <alcosharley21@gmail.com>
2024-10-08 09:55:03 +0800

    updated

---
+++ js/slider.js
@@ -0,0 +1,22 @@
+var slider = tns({
+  container: ".services__slider",
+  items: 3,
+  speed: 1000,
+  gutter: 100,
+  slideBy: "1",
+  autoplay: false,
+  navPosition: "top",
+  containerControls: "#controls",
+  prevButton: ".prev",
+  nextButton: ".next",
+  responsive: {
+    1250: {
+      items: 3,
+      // gutter:100,
+    },
+    200: {
+      items: 2,
+      gutter:25,
+    },
+  },
+});
